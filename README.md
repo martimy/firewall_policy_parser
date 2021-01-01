7c1193c30d25af91c1753bb86af31aa760918416
Maen Artimy <artimym@gmail.com>
2021-01-01 16:14:05 -0400

    several code improvements

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Firewall Policy Parser
+
 A Python application that parses a firewall configuration and converts firewall policies into table format for easy reading and analysis.
+
 Currently, the parser works with [Fortinet](https://www.fortinet.com/)'s Forigate configuration (FortiOS) only.
 
 ## Requirements
@@ -17,9 +19,15 @@ python3 -m pip install pyparsing
 ## How to Use
 
 ```
-python3 fg_policy_parser_lite.py Example.conf > out.csv
+python3 fg_policy_parser_lite.py Example.conf
 ```
 
-Which produces a table like this:
+Which saves the results in file 'out.csv'.
 
 ![Policies](images/policies1.png)
+
+Optionally, you may specify the output file name:
+
+```
+python3 fg_policy_parser_lite.py Example.conf policies
+```
--- fg_policy_parser_lite.py
+++ fg_policy_parser_lite.py
@@ -19,7 +19,7 @@ import sys
 
 # This app will be able to parse FG policies like the following example:
 
-example = r"""\
+cfg = r"""\
 config firewall policy
     edit 93
         set uuid 3509d226-cff8-51e5-aa5c-35c83c70e195
@@ -36,10 +36,10 @@ config firewall policy
 end
 """
 
-# Constants
+outfilename = "out.csv"
 FW_POLICY_SECTION = "config firewall policy\n"
 
-# General Definitions
+# General Parsing Definitions
 SEPERATOR = Word("-_. ", max=1)
 LBRACE, RBRACE, SEMI, QUOTE, COMMA = map(Suppress, '{};",')
 
@@ -57,7 +57,7 @@ def policySectionParser(text):
 
 def policyParser(text):
     """
-    Parse individual firewall policy.
+    Parse individual firewall policies.
     """
 
     NEXTMARK = Suppress(Keyword("next"))
@@ -70,62 +70,101 @@ def policyParser(text):
     policyParam = objName | objList | quotedString | SkipTo("\n")
     policyStatement = Literal("set").suppress() + \
         Group(fieldName + policyParam)
-    policyDef = policyNum + \
-        Dict(OneOrMore(policyStatement)) + NEXTMARK.suppress()
+    policyDef = policyNum + Dict(OneOrMore(policyStatement)) + NEXTMARK
 
     return policyDef.searchString(text)
 
 
-def fieldNames(policy_sections):
+def policyFinder(text):
+    """
+    Finds individual firewall policies by locating the keywords 'edit' and 'next'.
+    It is used to verify the number of policies parsed correctly.
+    """
+
+    policyNum = Literal("edit").suppress() + Word(nums)
+    policyDef = policyNum + SkipTo("next").suppress()
+
+    return policyDef.searchString(text)
+
+
+def fieldNames(policies):
     """
     Extract field names from parsed policies
     """
 
     fields = set()
-    for section in policy_sections:
-        if section.content:
-            for obj in policyParser(section.content):
-                fields.update(obj.asDict().keys())
+    for sec in policies:
+        for obj in sec:
+            fields.update(obj.asDict().keys())
     return list(fields)
 
 
-def policyToString(columns, policy_sections):
+def verifyParsing(policyNums, policies):
     """
-    Returns a list of strings. Each string represent a command-seperated table of firewall policies.
+    Generate report for debugging.
+    """
+
+    num = len(policyNums)
+    st = f"Found {num} firewall policy section(s).\n"
+
+    for x in range(num):
+        st += f"Found {len(policyNums[x])} and parsed {len(policies[x])} policies in section {x}.\n"
+
+    # for x in range(num):
+    #     st += f"Policy numbers found in section { x } (in order):\n"
+    #     st += ",".join([item[0] for item in policyNums[x]]) + "\n"
+
+    for x in range(num):
+        notparsed = set([ item[0] for item in policyNums[x]]) - set([ item[0] for item in policies[x]])
+        if notparsed:
+            st += f"Policies not parsed in section { x }: "
+            st += ",".join(list(notparsed)) + "\n"
+
+    return st
+
+
+def policyToCSV(columns, policies):
+    """
+    Returns a list of strings. 
+    Each string represents a comma-seperated table of a single firewall policy section.
     """
 
     sections = []
-    for section in policy_sections:
-        if section.content:
-            order = 0
-            st = ','.join(columns) + "\n"
-            for obj in policyParser(section.content):
-                order += 1
-                row = [obj.get(att, '') for att in columns]
-                row[0] = str(order)
-                st += ",".join(row) + "\n"
-            sections.append(st)
+    for sec in policies:
+        # order = 0
+        st = ','.join(columns) + "\n"
+        for obj in sec:
+            # order += 1
+            row = [obj.get(att, '') for att in columns]
+            # row[0] = str(order)
+            st += ",".join(row) + "\n"
+        sections.append(st)
     return sections
 
 
 if __name__ == "__main__":
-    # Read a Fortinet configuration file or exit
-    if(len(sys.argv) > 1):
-        fgfile = open(sys.argv[1].strip(), 'r')
+    # Read command line arguments
+    if len(sys.argv) > 2:
+        outfilename = sys.argv[2].split('.')[0] + ".csv"
+    if len(sys.argv) > 1:
+        fgfile = open(sys.argv[1], 'r')
         cfg = fgfile.read()
     else:
-        print("Input file name is required!")
-        print("An example is used for now.")
-        cfg = example
+        print("Input file name is required! An example is used for now.")
 
     # Parse firewall policies
     policySections = policySectionParser(cfg)
+    policyNums = [policyFinder(section) for section in policySections]
+    policies = [policyParser(section.content) for section in policySections]
 
-    # Get field names from parsed policies
-    columns = sorted(fieldNames(policySections))
-    columns.insert(0, "order")
+    # Get field names from parsed policies and insert order field
+    columns = sorted(fieldNames(policies))
+    # columns.insert(0, "order")
 
-    # Convert parsed policies to tables
-    sections = policyToString(columns, policySections)
-    for s in sections:
-        print(s)
+    # Convert parsed policies to CSV and save to file
+    with open(outfilename, 'w') as outfile:
+        for s in policyToCSV(columns, policies):
+            outfile.write(s)
+        
+    # Print report
+    print(verifyParsing(policyNums, policies), file=sys.stderr)
